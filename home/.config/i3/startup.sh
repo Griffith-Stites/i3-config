3f5c19747445a81a73323eeb31ad9ced8f5876a8
mstites <maeve@mstites.com>
2020-12-01 06:31:31 -0500

    Fix deskop startup issues

--- home/.config/i3/startup.sh
+++ home/.config/i3/startup.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
+/bin/bash
 wal -R
+syncthing -no-browser
 bluetoothctl power on
 systemctl --user start mpris-proxy
 xrdb -merge ~/.Xresources
-redshift
-nm-applet
-xfce4-power-manager
-syncthing -no-browser
-redshift -t 6500:4000
+redshift-gtk -t 6500:4000
 imwheel -b 45
-xfce4-power-manager
-clipit
