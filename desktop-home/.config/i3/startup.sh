4b0c395578773362222e1c487804e7b8e20997d7
mstites <maeve@mstites.com>
2022-06-23 10:01:50 -0700

    Remove redshift from startup sh script

--- desktop-home/.config/i3/startup.sh
+++ desktop-home/.config/i3/startup.sh
@@ -3,9 +3,7 @@ wal -R
 bluetoothctl power on
 systemctl --user start mpris-proxy
 xrdb -merge ~/.Xresources
-redshift
 nm-applet
 xfce4-power-manager
 syncthing -no-browser
-redshift -t 6500:3500
 imwheel -b 45
