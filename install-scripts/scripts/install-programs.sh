5e1d99d0f9ffb6ebb068edb588a6685008a3ed82
mstites <maeve@mstites.com>
2022-07-12 17:28:01 -0400

    Seperate device specific application options

--- install-scripts/scripts/install-programs.sh
+++ install-scripts/scripts/install-programs.sh
@@ -14,16 +14,29 @@ if [ $x == 1 ]; then
   echo -e "\nStarting pacman installs:"
   sudo pacman -Su qutebrowser pass rofi ranger python-pywal r bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth jupyterlab python syncthing firefox chromium libreoffice-fresh gimp pdfarranger inkscape transmission-gtk openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
   echo -e "\nSwitching to AUR installs via 'yay' AUR helper:"
-  yay -Su franz-bin rstudio-desktop-bin spotify notion-app chromium-widevine zoom logiops nordvpn-bin pulseffects-legacy
+  yay -Su franz-bin rstudio-desktop-bin spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
 
 elif [ $x == 2 ]; then
   echo "Minimal install"
   echo -e "\nStarting pacman installs:"
-  sudo pacman -Su qutebrowser pass rofi ranger python-pywal r bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth python syncthing firefox chromium gimp pdfarranger openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
+  sudo pacman -Su qutebrowser pass rofi ranger python-pywal bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth python syncthing firefox chromium gimp pdfarranger openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
   echo -e "\nSwitching to AUR installs via 'yay' AUR helper:"
-  yay -Su franz-bin spotify notion-app chromium-widevine zoom logiops nordvpn-bin pulseffects-legacy
+  yay -Su franz-bin spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
  
 else
   echo "ERROR: Number not in selection range. Exiting install"
   exit
+fi
+
+echo -ne "Install device specific programs [1-4]? [1. Laptop, 2. Desktop, 3. Surface, 4. None]"
+read x
+if [ $x == 1]; then
+  echo "Laptop specific applications"
+  echo "No current laptop specific applications"
+elif [ $x == 2]; then
+  echo "Desktop specific applications"
+  sudo pacman -Su logiops
+elif [ $x == 3]; then
+  echo "Surface specific applications"
+  echo "NOT YET IMPLEMENTED"
 fi
