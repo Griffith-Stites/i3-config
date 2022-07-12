6f5f80d8e7ee11838aec15813b6eea54bba596f1
mstites <maeve@mstites.com>
2022-07-12 16:55:07 -0400

    Add logid to startup and move redshift to systemctl

--- desktop-home/.config/i3/startup.sh
+++ desktop-home/.config/i3/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 wal -R
 bluetoothctl power on
-systemctl --user restart mpris-proxy
-systemctl --user restart redshift
+systemctl --user start mpris-proxy
 xrdb -merge ~/.Xresources
 nm-applet
 xfce4-power-manager
 syncthing -no-browser
 imwheel -b 45
+logid -c .config/logid/logid.cfg
