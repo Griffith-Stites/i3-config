fe8d6695576216d2d5f8e8697377231611d37909
mstites <maeve@mstites.com>
2022-09-13 10:36:25 -0400

    Add termdown and potato to install programs

--- install-scripts/scripts/install-programs.sh
+++ install-scripts/scripts/install-programs.sh
@@ -9,19 +9,21 @@ sudo pacman -R palemoon-bin mousepad hexchat deluge epdfview xviewer
 # Installer
 echo -ne "Install programs for development [1-3]: 1. Yes | 2. No | 3. Quit install \n"
 read x
+# Should change these so there is just an extra option for dev tools. That way we aren't double listing.
+# Add:  termdown (pacman), potato/ordne (yay1)
 if [ $x == 1 ]; then
   echo "Full install, including development tools"
   echo -e "\nStarting pacman installs:"
-  sudo pacman -Su qutebrowser pass rofi ranger python-pywal r bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth jupyterlab python syncthing firefox chromium libreoffice-fresh gimp pdfarranger inkscape transmission-gtk openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
+  sudo pacman -Su qutebrowser pass rofi ranger termdown python-pywal r bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth jupyterlab python syncthing firefox chromium libreoffice-fresh gimp pdfarranger inkscape transmission-gtk openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
   echo -e "\nSwitching to AUR installs via 'yay' AUR helper:"
-  yay -Su franz-bin rstudio-desktop-bin spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
+  yay -Su franz-bin potato rstudio-desktop-bin spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
 
 elif [ $x == 2 ]; then
   echo "Minimal install"
   echo -e "\nStarting pacman installs:"
   sudo pacman -Su qutebrowser pass rofi ranger python-pywal bluez trash-cli bluez-utils pulseaudio pulseaudio-bluetooth python syncthing firefox chromium gimp pdfarranger openvpn sshfs python-adblock pavucontrol scrot redshift atril ttf_apple_emoji bash-completion thunderbird clipit speedtest-cli gvim evince
   echo -e "\nSwitching to AUR installs via 'yay' AUR helper:"
-  yay -Su franz-bin spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
+  yay -Su franz-bin potato spotify notion-app chromium-widevine zoom nordvpn-bin pulseffects-legacy
  
 elif [ $x == 3 ]; then
   echo "Exiting Install"
